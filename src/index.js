e46cb13f934f4f46b4ee5b412f714100bc447ae0
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2020-10-02 20:53:40 +0300

    add: functions logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let a = 0;
+  if(!array || array.length === 0){
+    a = 0;
+    return a;
+  }
+  array.forEach(element => {
+    if(element < a){
+      a = element;
+    }
+  });
+  return a;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let a = 0;
+  if(!array || array.length === 0){
+    a = 0;
+    return a;
+  }
+  array.forEach(element => {
+    if(element > a){
+      a = element;
+    }
+  });
+  return a;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let a = 0;
+  if(!array || array.length === 0){
+    a = 0;
+    return a;
+  }
+  array.forEach(element => {
+    a += element;
+  });
+  a = a / array.length;
+  return a;
 }
